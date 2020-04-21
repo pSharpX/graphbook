fc39e1a2c33f6083bda216d7969be58e281acbf5
pSharpX <guardian2093@gmail.com>
2020-04-21 18:23:18 -0500

    clean code

--- src/client/App.js
+++ src/client/App.js
@@ -3,55 +3,12 @@ import { Helmet } from "react-helmet";
 import "../assets/css/style.css";
 import Feed from "./Feed";
 import Feed2 from "./Feed2";
-
-const posts = [
-  {
-    id: 2,
-    text: "Lorem ipsun",
-    user: {
-      avatar: "/uploads/avatar1.png",
-      username: "Test User",
-    },
-  },
-  {
-    id: 1,
-    text: "Lorem ipsun",
-    user: {
-      avatar: "/uploads/avatar2.png",
-      username: "Test User",
-    },
-  },
-];
+import Chats from "./Chats";
 
 export default class App extends Component {
-  state = {
-    posts: posts,
-    postContent: "",
-  };
-
-  handlePostContentChange = (event) => {
-    this.setState({ postContent: event.target.value });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const newPost = {
-      id: this.state.posts.length + 1,
-      text: this.state.postContent,
-      user: {
-        avatar: "/uploads/avatar1.png",
-        username: "Fake User",
-      },
-    };
-
-    this.setState((prevState) => ({
-      posts: [newPost, ...prevState.posts],
-      postContent: "",
-    }));
-  };
+  state = {};
 
   render() {
-    const { posts, postContent } = this.state;
     return (
       <div className="container">
         <Helmet>
