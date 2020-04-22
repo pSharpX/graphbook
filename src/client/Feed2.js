3a3ea0ae031865fdb9a543ac145eb0f1a82a351e
pSharpX <guardian2093@gmail.com>
2020-04-22 14:07:46 -0500

    pagination with apollo client
    consuming service with pagination

--- src/client/Feed2.js
+++ src/client/Feed2.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import { Query, Mutation } from "react-apollo";
+import InfiniteScroll from "react-infinite-scroller";
 import gql from "graphql-tag";
 import "../assets/css/style.css";
 
 const GET_POSTS = gql`
-  {
-    posts {
-      id
-      text
-      user {
-        avatar
-        username
+  query postsFeed($page: Int, $limit: Int) {
+    postsFeed(page: $page, limit: $limit) {
+      posts {
+        id
+        text
+        user {
+          avatar
+          username
+        }
       }
     }
   }
@@ -32,6 +35,9 @@ const ADD_POST = gql`
 export default class Feed2 extends Component {
   state = {
     postContent: "",
+    hasMore: true,
+    page: 0,
+    limit: 10,
   };
 
   handlePostContentChange = (event) => {
@@ -51,17 +57,51 @@ export default class Feed2 extends Component {
     };
   };
 
+  loadMore = (fetchMore) => () => {
+    const self = this;
+    const { page } = this.state;
+    fetchMore({
+      variables: {
+        page: page + 1,
+      },
+      updateQuery(previousResult, { fetchMoreResult }) {
+        if (!fetchMoreResult.postsFeed.posts.length) {
+          self.setState({ hasMore: false });
+          return previousResult;
+        }
+        self.setState({ page: page + 1 });
+        const newData = {
+          postsFeed: {
+            __typename: "PostFeed",
+            posts: [
+              ...previousResult.postsFeed.posts,
+              ...fetchMoreResult.postsFeed.posts,
+            ],
+          },
+        };
+        return newData;
+      },
+    });
+  };
+
   render() {
-    const { postContent } = this.state;
+    const { postContent, hasMore, limit } = this.state;
     return (
       <div className="container">
         <div className="postForm">
           <Mutation
             // refetchQueries={[{ query: GET_POSTS }]}
             update={(store, { data: { addPost } }) => {
-              const data = store.readQuery({ query: GET_POSTS });
-              data.posts.unshift(addPost);
-              store.writeQuery({ query: GET_POSTS, data });
+              const variables = {
+                page: 0,
+                limit,
+              };
+              const data = store.readQuery({
+                query: GET_POSTS,
+                variables,
+              });
+              data.postsFeed.posts.unshift(addPost);
+              store.writeQuery({ query: GET_POSTS, variables, data });
             }}
             optimisticResponse={{
               __typename: "mutation",
@@ -90,27 +130,44 @@ export default class Feed2 extends Component {
             )}
           </Mutation>
         </div>
-        <div className="feed">
-          <Query pollInterval={5000} query={GET_POSTS}>
-            {({ loading, error, data }) => {
-              if (loading) return "Loading...";
-              if (error) return error.message;
-              const { posts } = data;
-              return posts.map((post, i) => (
-                <div
-                  key={post.id}
-                  className={"post" + (post.id < 0 ? " optimistic" : "")}
+        <Query
+          // pollInterval={5000}
+          query={GET_POSTS}
+          variables={{ page: 0, limit }}
+        >
+          {({ loading, error, data, fetchMore }) => {
+            if (loading) return "Loading...";
+            if (error) return error.message;
+            const { postsFeed } = data;
+            const { posts } = postsFeed;
+            return (
+              <div className="feed">
+                <InfiniteScroll
+                  loadMore={this.loadMore(fetchMore)}
+                  hasMore={hasMore}
+                  loader={
+                    <div className="loader" key="loader">
+                      Loading...
+                    </div>
+                  }
                 >
-                  <div className="header">
-                    <img src={post.user.avatar} />
-                    <h2>{post.user.username}</h2>
-                  </div>
-                  <p className="content">{post.text}</p>
-                </div>
-              ));
-            }}
-          </Query>
-        </div>
+                  {posts.map((post, i) => (
+                    <div
+                      key={post.id}
+                      className={"post" + (post.id < 0 ? " optimistic" : "")}
+                    >
+                      <div className="header">
+                        <img src={post.user.avatar} />
+                        <h2>{post.user.username}</h2>
+                      </div>
+                      <p className="content">{post.text}</p>
+                    </div>
+                  ))}
+                </InfiniteScroll>
+              </div>
+            );
+          }}
+        </Query>
       </div>
     );
   }
